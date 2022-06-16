f17cf4e4218163d2cf83c0042ea2354382424f7b
Junior <junior.jr.jl@gmail.com>
2022-06-16 14:33:46 -0300

    criação do projeto

---
+++ src/main/java/br/com/dio/reactiveflashcards/ReactiveFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.reactiveflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/reactiveflashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.reactiveflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
