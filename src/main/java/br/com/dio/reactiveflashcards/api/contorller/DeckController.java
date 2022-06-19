ca06cdb5bf1c691f0cce43df7350ec304650f2ae
Junior <junior.jr.jl@gmail.com>
2022-06-19 18:58:06 -0300

    endpoints de exclusão e alteração de decks

--- src/main/java/br/com/dio/reactiveflashcards/api/contorller/DeckController.java
+++ src/main/java/br/com/dio/reactiveflashcards/api/contorller/DeckController.java
@@ -9,9 +9,11 @@ import br.com.dio.reactiveflashcards.domain.service.query.DeckQueryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +23,7 @@ import reactor.core.publisher.Mono;
 import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Validated
@@ -47,6 +50,22 @@ public class DeckController {
         return deckQueryService.findById(id)
                 .doFirst(() -> log.info("==== Finding a deck with follow id {}", id))
                 .map(deckMapper::toResponse);
+    }
+
+    @PutMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE, value = "{id}")
+    public Mono<DeckResponse> update(@PathVariable @Valid @MongoId(message = "{deckController.id}") final String id,
+                                     @Valid @RequestBody final DeckRequest request){
+        return deckService.update(deckMapper.toDocument(request, id))
+                .doFirst(() -> log.info("==== Updating a deck with follow info [body: {}, id: {}]", request, id))
+                .map(deckMapper::toResponse);
+    }
+
+
+    @DeleteMapping(value = "{id}")
+    @ResponseStatus(NO_CONTENT)
+    public Mono<Void> delete(@PathVariable @Valid @MongoId(message = "{deckController.id}") final String id){
+        return deckService.delete(id)
+                .doFirst(() -> log.info("==== Deleting a user with follow id {}", id));
     }
 
 }
--- src/main/java/br/com/dio/reactiveflashcards/domain/service/DeckService.java
+++ src/main/java/br/com/dio/reactiveflashcards/domain/service/DeckService.java
@@ -2,6 +2,7 @@ package br.com.dio.reactiveflashcards.domain.service;
 
 import br.com.dio.reactiveflashcards.domain.document.DeckDocument;
 import br.com.dio.reactiveflashcards.domain.repository.DeckRepository;
+import br.com.dio.reactiveflashcards.domain.service.query.DeckQueryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,27 @@ import reactor.core.publisher.Mono;
 public class DeckService {
 
     private final DeckRepository deckRepository;
+    private final DeckQueryService deckQueryService;
 
     public Mono<DeckDocument> save(final DeckDocument document){
         return deckRepository.save(document)
                 .doFirst(() -> log.info("==== Try to save a follow deck {}", document));
+    }
+
+    public Mono<DeckDocument> update(final DeckDocument document){
+        return deckQueryService.findById(document.id())
+                .map(deck -> document.toBuilder()
+                        .createdAt(document.createdAt())
+                        .updatedAt(document.updatedAt())
+                        .build())
+                .flatMap(deckRepository::save)
+                .doFirst(() -> log.info("==== Try to update a deck with follow info {}", document));
+    }
+
+    public Mono<Void> delete(final String id){
+        return deckQueryService.findById(id)
+                .flatMap(deckRepository::delete)
+                .doFirst(() -> log.info("==== Try to delete a user with follow id {}", id));
     }
 
 }
